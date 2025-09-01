76b568dbe6d4bc3c5460e19af2f02656ada96372
neo <javohirtech@gmail.com>
2025-09-01 14:47:59 +0500

    feat: Implement tray icon functionality and update app window settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
 import "./App.css"
+import { Menu } from "@tauri-apps/api/menu";
+import { TrayIcon, TrayIconOptions } from '@tauri-apps/api/tray';
+import { useEffect } from "react";
+
+const menu = await Menu.new({
+  items: [
+    {
+      id: 'quit',
+      text: 'Quit',
+      action: () => {
+        console.log("salom")
+      }
+    },
+  ],
+});
+
+const options: TrayIconOptions = {
+  menu,
+  menuOnLeftClick: true,
+};
+
+
 const App = () => {
-  return (
-    <div className="h-screen flex items-center justify-center w-full">
-      <h1>salomsalom</h1>
-    </div>
-  )
+  const initTray = async () => {
+    const tray = await TrayIcon.new(options);
+    tray.setTitle(String(new Date().getMilliseconds()))
+  }
+
+  useEffect(() => {
+    initTray()
+  }, [])
+
+
+  return ""
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,6 @@
-import { Menu } from "@tauri-apps/api/menu";
-import { TrayIcon, TrayIconOptions } from '@tauri-apps/api/tray';
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
-const menu = await Menu.new({
-  items: [
-    {
-      id: 'quit',
-      text: 'Quit',
-      action: () => {
-        console.log("salom")
-      }
-    },
-  ],
-});
-
-const options: TrayIconOptions = {
-  menu,
-  menuOnLeftClick: true,
-};
-
-const tray = await TrayIcon.new(options);
-tray.setTitle("salom")
-tray.setIcon("./32x32.png")
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
