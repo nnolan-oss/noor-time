8819e77d1376be7e9ae9650b5e72a19cb39084d1
neo <javohirtech@gmail.com>
2025-09-01 13:27:47 +0500

    feat: Add Gradle wrapper and configuration files for Android project
    - Created gradle-wrapper.properties to specify Gradle distribution.
    - Added gradlew shell script for Unix-based systems to execute Gradle tasks.
    - Included gradlew.bat for Windows users to run Gradle commands.
    - Established settings.gradle to include the app module and apply Tauri settings.
    - Added 32x32 PNG icon for the application.
    - Included Vite SVG logo for branding purposes.

---
+++ src-tauri/gen/android/app/src/main/java/com/nnolan/noor_time/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nnolan.noor_time
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
